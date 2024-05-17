414175ff9457a9b1c818d8af51c9026fe51f4b93
sandratraniaina <sandratrarafa@gmail.com>
2024-05-17 09:02:42 +0300

    feat: Create getArgsClasses  in ReflectUtils

--- src/utils/ReflectUtils.java
+++ src/utils/ReflectUtils.java
@@ -1,5 +1,14 @@
 package utils;
 
 public class ReflectUtils {
-    
+    public static Class<?>[] getArgsClasses(Object... args) {
+        Class<?>[] classes = new Class[args.length];
+        int i = 0;
+        for (Object object : args) {
+            classes[i] = object.getClass();
+            i++;
+        }
+        return classes;
+    }
+
 }
