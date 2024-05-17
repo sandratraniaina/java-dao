5c2df6b4a38964eba906303a078ecc4239a45cfb
sandratraniaina <sandratrarafa@gmail.com>
2024-05-17 08:55:34 +0300

    Create enclose method in  StringUtils

--- src/utils/StringUtils.java
+++ src/utils/StringUtils.java
@@ -4,4 +4,8 @@ public class StringUtils {
     public static String capitalize(String str) {
         return str.substring(0, 1).toUpperCase() + str.substring(1);
     }
+
+    public static String enclose(String str, String enclosure) {
+        return enclosure + str + enclosure;
+    }
 }
