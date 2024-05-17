eb5b895fa9222124b4b075739ec57301ccd9a3ca
sandratraniaina <sandratrarafa@gmail.com>
2024-05-17 09:07:36 +0300

    style: Formatting StringUtils and ReflectUtils classes code

--- src/utils/ReflectUtils.java
+++ src/utils/ReflectUtils.java
@@ -8,19 +8,23 @@ public class ReflectUtils {
     public static ArrayList<String> getAttributeNames(Object object) {
         Field[] f = object.getClass().getDeclaredFields();
         ArrayList<String> attrList = new ArrayList<String>();
+
         for (int i = 0; i < f.length; i++) {
             attrList.add(f[i].getName());
         }
+        
         return attrList;
     }
 
     public static Class<?>[] getArgsClasses(Object... args) {
         Class<?>[] classes = new Class[args.length];
         int i = 0;
+
         for (Object object : args) {
             classes[i] = object.getClass();
             i++;
         }
+
         return classes;
     }
 
--- src/utils/StringUtils.java
+++ src/utils/StringUtils.java
@@ -1,6 +1,7 @@
 package utils;
 
 public class StringUtils {
+
     public static String capitalize(String str) {
         return str.substring(0, 1).toUpperCase() + str.substring(1);
     }
@@ -8,4 +9,5 @@ public class StringUtils {
     public static String enclose(String str, String enclosure) {
         return enclosure + str + enclosure;
     }
+    
 }
