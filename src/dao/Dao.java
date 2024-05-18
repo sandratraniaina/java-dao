6224fa1c01b16cc10dd0aa642a683e2cd05ffab4
sandratraniaina <sandratrarafa@gmail.com>
2024-05-18 21:06:22 +0300

    feat: Create Dao base class

---
+++ src/dao/Dao.java
@@ -0,0 +1,11 @@
+package dao;
+
+import dao.exception.DaoException;
+import sql.criteria.Pagination;
+import sql.criteria.Range;
+import sql.SQLBuilder;
+import utils.SQLUtils;
+
+public class Dao {
+
+}
