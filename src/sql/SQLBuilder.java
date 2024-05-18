e55fb1c6b0876e07f1df9641bb84805a5192ac16
sandratraniaina <sandratrarafa@gmail.com>
2024-05-18 20:53:53 +0300

    Create SQLBuilder base class

---
+++ src/sql/SQLBuilder.java
@@ -0,0 +1,11 @@
+package sql;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+import utils.ArrayUtils;
+
+public class SQLBuilder {
+    
+}
